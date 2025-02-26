de323cfbb59116ef8bb5b3ff70818cd733c72fde
jordi.bru <jordi.bru@mango.com>
2025-02-26 10:51:30 +0100

    update lazygit, catppuccin theme and plugins added

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -8,5 +8,13 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- import/override with your plugins folder
 }
